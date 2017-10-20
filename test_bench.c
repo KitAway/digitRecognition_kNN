8a2d3f7a3c358ccccc558a681ce388a7f934f5f9
liang@fedora <liang.ma@studenti.polito.it>
2017-10-20 13:51:36 +0200

    reformated the codes

--- test_bench.c
+++ test_bench.c
@@ -7,95 +7,95 @@
 #include "topK.h"
 int main()
 {
-        int success_count =0;
-        int dist[K],r_dist[K];
-        int index[K],r_index[K];
-//step1:define two bool(0 or 1) type two-dimensional-array
-	bool train_code[N_TRAIN][1024];
-	bool test_code[1024];
-//step2:define the distance_long array
+	int success_count =0;
+	int dist[K],r_dist[K];
+	int index[K],r_index[K];
+	//step1:define two bool(0 or 1) type two-dimensional-array
+	bool train_code[N_TRAIN][IMAGE_SIZE];
+	bool test_code[IMAGE_SIZE];
+	//step2:define the distance_long array
 	int distances[N_DIST];
 
-	char testdigit[N_TEST];
-	
-//step3:read the train files and test files
-         readtraincode(train_code);
+	char testdigit[]="testDigits/10_2000.txt";
+
+	//step3:read the train files and test files
+	readtraincode(train_code);
 
 
-//step4:calculate the distance between the train files and each test file
-     /*   for(int i=0; i<N_TEST;i++)
-        {*/
-    for(int x = 0; x<10;x++)
-    {
-        for(int y = 0; y<M_TEST;y++)
-        {
-	    sprintf(testdigit,"/home/guo/Desktop/example/2017-10-20/DIGIT_RECOGNITION/testDigits/%d_%d.txt",x,y);
+	//step4:calculate the distance between the train files and each test file
+	for(int digit = 0; digit<NUM_CLASS;digit++)
+	{
+		for(int _order = 0; _order<M_TEST;_order++)
+		{
+			sprintf(testdigit,"testDigits/%d_%d.txt",digit,_order);
 
-            readtestcode(test_code, testdigit);
+			readtestcode(test_code, testdigit);
 
-	    init_distances(test_code,train_code,distances);
-            
-//step5:sort the first TOPK value in distance based on the init_distances array
-            for(int i=0; i<K; i++)
-            {
-                dist[i] = 0;
-                index[i] = 0;
-            }            
-            topk_distance(distances,r_dist,r_index);  
+			init_distances(test_code,train_code,distances);
 
-//step6:sort the topK value in the topK based on the distance array
-            topK(distances, dist, index);       
-     
-//step7.assign the topK values to the local array
-	   for(int i=0;i<K;i++)
-           {
-   //            printf("r_dist[%d]=%d\n",i,r_dist[i]);
-	   }
+			//step5:sort the first TOPK value in distance based on the init_distances array
+			for(int i=0; i<K; i++)
+			{
+				dist[i] = 0;
+				index[i] = 0;
+			}            
+			topk_distance(distances,r_dist,r_index);  
 
-	   for(int i=0;i<K;i++)
-           {
- //              printf("dist[%d]=%d\n",i,dist[i]);
-	
-	   }
+			//step6:sort the topK value in the topK based on the distance array
+			topK(distances, dist, index);       
 
-          int label[K],order[K];
-				int count[10]={0,0,0,0,0,0,0,0,0,0};
-				for(int i=0;i<K;i++)
-				{  
-					int j=-1;
-					label[i] = (r_index[i])/20;
-					order[i] = (r_index[i])%20;
+			//step7.assign the topK values to the local array
+			for(int i=0;i<K;i++)
+			{
+				//            printf("r_dist[%d]=%d\n",i,r_dist[i]);
+			}
+
+			for(int i=0;i<K;i++)
+			{
+				//              printf("dist[%d]=%d\n",i,dist[i]);
+
+			}
+
+			int label,order;
+			int count[NUM_CLASS]={0,0,0,0,0,0,0,0,0,0};
+			for(int i=0;i<K;i++)
+			{  
+				int j=-1;
+				label = (r_index[i])/M_TRAIN;
+				order = (r_index[i])%M_TRAIN;
 				//	printf("r_dist[topK_%d]=%d,r_index[topK_%d]=%d,label_order=%d_%d\n",i,r_dist[i],i,r_index[i],label[i],order[i]);
-					count[label[i]]++;
-                                 //       printf("--------------------\n");
+				count[label]++;
+				//       printf("--------------------\n");
 				//	printf("count[label[topk%d]=%d]=%d\n",i,label[i],count[label[i]]);
+			}
+			int max = count[0];
+			int index1 =0;
+			for(int i=0; i<NUM_CLASS;i++)
+			{   //    printf("count[i=%d]=%d\n",i,count[i]);
+				if(max < count[i])
+				{	
+					max = count[i];
+					index1 = i;
 				}
-				int max = count[0];
-				int index1 =0;
-				for(int i=0; i<10;i++)
-				{   //    printf("count[i=%d]=%d\n",i,count[i]);
-					if(max < count[i])
-					{	max = count[i];
-					        index1 = i;
-                                        }
-                                        
-                                        
-					//  printf("index1=%d,max=%d\n",index,max);
-				}
-				printf("x=%d,y=%d,index1=%d,appearance=%d,recognition percentage=%.0f%%\n",x,y,index1,max,100*max*1.0/K);
-                        //        printf("x=%d,index=%d\n",x,index1);
 
-                                if(x==index1)
-                                {
-                                  
-                                printf("x=%d=index1=%d,success\n",x,index1);
-                                 success_count ++;
-                                }
-       }
-                                
-    }           
-                                 printf("success_count2=%d\n",success_count);
-                                 printf("success rate=%.0f%%\n",100*success_count/N_TEST);
+
+				//  printf("index1=%d,max=%d\n",index,max);
+			}
+//			 printf("digit=%d,_order=%d,index1=%d,appearance=%d,recognition percentage=%.0f%%\n",digit,_order,index1,max,100*max*1.0/K);
+			//        printf("digit=%d,index=%d\n",digit,index1);
+
+			if(digit==index1)
+			{
+//				printf("file:%d_%d.txt,\tlabel=%d, guess digit=%d, success.\n",digit,_order,digit,index1);
+				success_count ++;
+			}
+			else 
+			{
+	//			printf("file:%d_%d.txt,\tlabel=%d, guess digit=%d, failed.\n",digit,_order,digit,index1);
+			}
+		}
+
+	}           
+	printf("success rate=%.0f%%\n",100*success_count/(N_TEST*1.0));
 	return 0;
 }
-
